08f4b98fd34635a111b48e7e20f6bc6a03fad8f7
mdeyo <mdeyo@mit.edu>
2017-05-01 20:59:47 -0400

    changes for invited speakers section and headshots

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,61 +4,102 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 
-	skel
-		.breakpoints({
-			desktop: '(min-width: 737px)',
-			tablet: '(min-width: 737px) and (max-width: 1200px)',
-			mobile: '(max-width: 736px)'
-		})
-		.viewport({
-			breakpoints: {
-				tablet: {
-					width: 1080
-				}
-			}
-		});
+(function ($) {
 
-	$(function() {
+    skel
+        .breakpoints({
+            desktop: '(min-width: 737px)',
+            tablet: '(min-width: 737px) and (max-width: 1200px)',
+            mobile: '(max-width: 736px)'
+        })
+        .viewport({
+            breakpoints: {
+                tablet: {
+                    width: 1080
+                }
+            }
+        });
 
-		var	$window = $(window),
-			$body = $('body');
+    $(function () {
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        var $window = $(window),
+            $body = $('body');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        $window.on('load', function () {
+            $body.removeClass('is-loading');
+        });
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
+        // Prioritize "important" elements on mobile.
+        skel.on('+mobile -mobile', function () {
+            $.prioritize(
+                '.important\\28 mobile\\29',
+                skel.breakpoint('mobile').active
+            );
+        });
 
-		// Scrolly.
-			$window.load(function() {
+        // CSS polyfills (IE<9).
+        if (skel.vars.IEVersion < 9)
+            $(':last-child').addClass('last-child');
 
-				var x = parseInt($('.wrapper').first().css('padding-top')) - 15;
+        // Scrolly.
+        $window.load(function () {
 
-				$('#nav a, .scrolly').scrolly({
-					speed: 1000,
-					offset: x
-				});
+            var x = parseInt($('.wrapper').first().css('padding-top')) - 15;
 
-			});
+            $('#nav a, .scrolly').scrolly({
+                speed: 1000,
+                offset: x
+            });
 
-	});
+            $('#portfolio a, .scrolly').scrolly({
+                speed: 1000,
+                offset: x
+            });
+
+        });
+
+    });
 
 })(jQuery);
